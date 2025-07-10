42a3466742d5a6f9b235301c917854dbe46b32da
Jith2 <121929065+Jith2@users.noreply.github.com>
2025-07-10 20:18:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,19 +3,8 @@ variable "aws_region" {
   type        = string
   default     = "ap-south-1"
 }
-
-variable "customer_buckets" {
-  description = "Map of customer S3 bucket names to subfolders"
-  type        = map(list(string))
-}
-
-variable "central_account_id" {
-  description = "AWS Account ID of the central account for cross-account access"
-  type        = string
-}
-
 variable "tags" {
-  description = "Tags to apply to the S3 bucket"
+  description = "Tags to apply to all buckets"
   type        = map(string)
   default     = {
     Application = "Campaign"
