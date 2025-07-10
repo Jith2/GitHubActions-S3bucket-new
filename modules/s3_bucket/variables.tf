1d81881a8b83ccf1ce394279f0f0283111eb40cf
Jith2 <121929065+Jith2@users.noreply.github.com>
2025-07-10 17:10:30 +0530

    Update variables.tf

--- modules/s3_bucket/variables.tf
+++ modules/s3_bucket/variables.tf
@@ -6,20 +6,10 @@ variable "bucket_name" {
 variable "subfolders" {
   description = "List of subfolders to create in the bucket"
   type        = list(string)
+  default     = ["customers/import/", "customers/export/"]
 }
-
 variable "tags" {
   description = "Tags to apply to the S3 bucket"
   type        = map(string)
   default     = {}
-}
-
-variable "central_account_id" {
-  description = "AWS Account ID of the central account for cross-account access"
-  type        = string
-}
-
-variable "iam_user_name" {
-  description = "IAM user name in the central account to allow access"
-  type        = string
 }
