a642fcbb16da4094d0a5f98536245a6c0cef3136
Jith2 <121929065+Jith2@users.noreply.github.com>
2025-07-10 20:13:15 +0530

    Update Updated-main.tf

--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -1,66 +1,63 @@
-locals {
-  all_folders = distinct(flatten([
-    for folder in var.subfolders : [
-      for i in range(length(split("/", trim(folder, "/")))) :
-        "${join("/", slice(split("/", trim(folder, "/")), 0, i + 1))}/"
-    ]
-  ]))
-}
-
 resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
+  bucket        = var.bucket_name
   force_destroy = true
-  tags   = var.tags
+  tags          = var.tags
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "this" {
-  count  = length(local.all_folders) > 0 ? 1 : 0
+resource "aws_s3_object" "folders" {
+  for_each = toset(var.subfolders)
+  bucket   = aws_s3_bucket.this.id
+  key      = each.value
+  content  = ""
+}
+
+locals {
+  central_user_arn = "arn:aws:iam::038697395377:user/tu-${var.bucket_name}"
+}
+ 
+resource "aws_s3_bucket_policy" "central_user_access" {
+  bucket = aws_s3_bucket.this.id
+ 
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AllowCentralIAMUserAccess",
+        Effect    = "Allow",
+        Principal = {
+          AWS = local.central_user_arn
+        },
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          aws_s3_bucket.this.arn,
+          "${aws_s3_bucket.this.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "expire_objects" {
   bucket = aws_s3_bucket.this.id
 
   dynamic "rule" {
-    for_each = local.all_folders
+    for_each = var.subfolders
     content {
-      id     = "expire-objects-30-days-${replace(trim(rule.value, "/"), "/", "-")}"
+      id     = "expire-objects-30-days-${replace(rule.value, "/", "-")}"
       status = "Enabled"
+
       filter {
         prefix = rule.value
       }
+
       expiration {
         days = 30
       }
     }
   }
-}
-
-resource "aws_s3_bucket_policy" "allow_central_user" {
-  bucket = aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.central_user_access.json
-}
-
-data "aws_iam_policy_document" "central_user_access" {
-  statement {
-    sid    = "AllowCentralAccountUserAccess"
-    effect = "Allow"
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${var.central_account_id}:user/${var.iam_user_name}"]
-    }
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:DeleteObject",
-      "s3:ListBucket"
-    ]
-    resources = [
-      aws_s3_bucket.this.arn,
-      "${aws_s3_bucket.this.arn}/*"
-    ]
-  }
-}
-
-resource "aws_s3_object" "subfolders" {
-  for_each = toset(local.all_folders)
-  bucket   = aws_s3_bucket.this.id
-  key      = each.value
-  content  = ""
 }
